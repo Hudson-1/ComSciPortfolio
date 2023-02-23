930bf9df0a44cb0e7a07f9d2741f62f4d62d2e35
Hudson <70992329+Hudson-1@users.noreply.github.com>
2023-02-23 10:14:40 -0800

    added landmarks app

---
+++ Landmarks/Landmarks/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Hudson Aibel on 10/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
