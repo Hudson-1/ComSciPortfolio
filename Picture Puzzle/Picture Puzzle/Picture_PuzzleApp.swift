fb8bc73e22bd9dd474392c055d186c0e617ea0c4
Hudson <70992329+Hudson-1@users.noreply.github.com>
2023-03-08 18:04:34 -0800

    puzzle project almost good

---
+++ Picture Puzzle/Picture Puzzle/Picture_PuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picture_PuzzleApp.swift
+//  Picture Puzzle
+//
+//  Created by Hudson Aibel on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Picture_PuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ImagePicker()
+        }
+    }
+}
---
+++ Picture Puzzle/Picture Puzzle/imagePicker.swift
@@ -0,0 +1,64 @@
+//
+//  imagePicker.swift
+//  Picture Puzzle
+//
+//  Created by Hudson Aibel on 3/8/23.
+//
+
+import SwiftUI
+import PhotosUI
+struct ImagePicker: View {
+    
+    @State var selectedItem: PhotosPickerItem?
+    @State var imageData: Data?
+    
+    @State var image: Image?
+    
+     
+    
+    var body: some View {
+        NavigationStack{
+            PhotosPicker(selection: $selectedItem,
+                         matching: .images,
+                         photoLibrary: .shared()){
+                Text("select an item")
+            }
+                         .onChange(of: selectedItem){ newItem in
+                             Task{
+                                 if let data = try? await newItem?.loadTransferable(type:Data.self){
+                                     if let uiImage = UIImage(data: data) {
+                                         if let croppedImage = cropSquare(uiImage){
+                                             if let resizedImage = resizeImage(image: croppedImage, targetSize: CGSize (width: 360, height: 260 )){
+                                                 
+                                                 image = Image(uiImage: resizedImage)
+                                                 imageData = resizedImage.pngData()
+                                             }
+                                             
+                            
+                                         }
+                                         
+                                        
+                                     }
+                                 }
+                             }
+                         }
+            if let image {
+                VStack{
+                    image
+                    
+                    NavigationLink("Puzzle", destination: PuzzleView(imageData: imageData!))
+                        .font(.title)
+                }
+            }
+            
+        
+            
+        }
+    }
+}
+
+struct imagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePicker()
+    }
+}
